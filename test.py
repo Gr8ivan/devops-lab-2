4e540a09c0972088f6c50bc3403b374745576708
Ivan <123373115+Gr8ivan@users.noreply.github.com>
2024-02-12 14:04:35 +0000

    fixed

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ num2 = input("Please enter number 2\n")
 
 avg = (int(num1)+int(num2))/2
 print("Average is : ", avg)
-if num1 >= num2:
+
+if num1 <= num2:
     print("Largest = " + num1 + "Smallest = " + num2)
 else:
     print("Largest = " + num2 + "Smallest = " + num1)
